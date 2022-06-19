291ea8e9037e16b18b5a73b1689df9b2b3eb2f61
inscentia <inscentia@yahoo.com>
2022-06-19 13:59:16 -0500

    setup mongodb cluster connection add .env file and connection string add ejs viewport remove index.html because it is no longer used. update .gitignore to include .env to protect any sensitive information

--- server.js
+++ server.js
@@ -1,42 +1,48 @@
 const express = require ('express');
 const app = express();
+const MongoClient = require('mongodb').MongoClient;
+const PORT = process.env.PORT || 8000; //use default environment port or 8000
+require('dotenv').config();
 const cors = require ('cors');
 
-const PORT = process.env.PORT || 8000; //use default environment port or 8000
 
+let db,
+    dbConnectionStr = process.env.DB_STRING,
+    dbName = 'myFirstDatabase'
+
+MongoClient.connect(dbConnectionStr, { useUnifiedTopology: true })
+    .then(client => {
+        console.log(`Connected to ${dbName} Database`)
+        db = client.db(dbName)
+    })
+
+
+app.set('view engine', 'ejs')
 app.use(cors()) //use cors
 app.use(express.static('public')) //tells express to load files from 'public' folder
+app.use(express.urlencoded({ extended: true }))
+app.use(express.json())
 
-
-//json object
-const philoList = {
-    'nietzsche':{
-        'Full Name': 'Friedrich Nietzsche',
-        'Nationality': 'German',
-        'Born': new Date('October 15, 1844'),
-        'Died': new Date('August 25 1900'),
-        'Main Interests': 'Aesthetics, philology, ethics, metaphysics, ontology, philosophy of history, poetry, religion, tragedy, truth, theory, value theory'
-    },
-    'hegel':{
-        'Full Name': 'Georg Wilhelm Friedrich Hegel',
-        'Nationality': 'German',
-        'Born': new Date('August 27 1770'),
-        'Died': new Date('November 14 1831'),
-        'Main Interests': 'Metaphysics, Epistemology, Naturphilosophie, Theology, Philosophy of history, Ethics, Political philosophy, Logic, Aesthetics'
-    },
-    'unknown': {
-    'age': 'UNKNOWN',
-    'birthName': 'UNKNOWN',
-    'birthLocation': 'UNKNOWN',
-    }
-}
-
-app.get('/', (req, res)=>{
-    res.sendFile(__dirname + '/index.html') 
-    //__dirname means file is in the same directory
+app.get('/', (req, res) => {
+    db.collection('Philosophers').find().toArray()
+    .then(data => {
+        console.log('DB data recieved')
+        res.render('index.ejs', { info: data })
+    })
+    .catch(error => console.error(error))
 })
 
+// app.get('/', (req, res)=>{
+//     db.collection('Philosophers').find().toArray()
+//     .then(data => {
+//         console.log(data)
+//         // response.json('index.ejs', { info: data })
+//     })
+//     .catch(error => console.error(error))
+// })
+
 //send json back; /api:queryParameter
+
 app.get ('/api/:philoName', (req, res)=>{ //get url and query param
     const philoName = req.params.philoName.toLowerCase() //assign query param to variable
     if(philoList[philoName]){ //if List contains query param variable (philosopher last name)
@@ -46,7 +52,6 @@ app.get ('/api/:philoName', (req, res)=>{ //get url and query param
     }
 })
 
-// app.listen(process.env.PORT, ()=>{
 app.listen(PORT, ()=>{
     console.log(`server is running on port ${PORT}`) //backticks to make ${} work
 })
