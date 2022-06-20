2141a7c966e523acabe17c54da6ce7a0383337ed
inscentia <inscentia@yahoo.com>
2022-06-20 16:00:32 -0500

    add functionality to add new philosophers to database begin to add functionality to delete documents from database

--- server.js
+++ server.js
@@ -23,35 +23,48 @@ app.use(express.static('public')) //tells express to load files from 'public' fo
 app.use(express.urlencoded({ extended: true }))
 app.use(express.json())
 
+//routes
+
 app.get('/', (req, res) => {
     db.collection('Philosophers').find().toArray()
     .then(data => {
-        console.log('DB data recieved')
+        console.log('DB data received')
         res.render('index.ejs', { info: data })
     })
     .catch(error => console.error(error))
 })
 
-// app.get('/', (req, res)=>{
-//     db.collection('Philosophers').find().toArray()
-//     .then(data => {
-//         console.log(data)
-//         // response.json('index.ejs', { info: data })
-//     })
-//     .catch(error => console.error(error))
-// })
-
-//send json back; /api:queryParameter
-
-app.get ('/api/:philoName', (req, res)=>{ //get url and query param
-    const philoName = req.params.philoName.toLowerCase() //assign query param to variable
-    if(philoList[philoName]){ //if List contains query param variable (philosopher last name)
-        res.json(philoList[philoName])    //respond with corresponding object
-    }else{ //if List contains no such name, res with unknown object.
-        res.json(philoList['unknown'])
-    }
+app.post('/addPhilos', (req, res) => {
+    db.collection('Philosophers').insertOne({lastName: req.body.lastName.toLowerCase(),
+    fullName: req.body.fullName, born: req.body.born, died: req.body.died,
+    mainInterests: req.body.mainInterests})
+    .then(result => {
+        console.log('Data added. Thank you.')
+        res.redirect('/')
+    })
+    .catch(error => console.error(error))
 })
 
+app.delete('/deleteRapper', (request, response) => {
+    db.collection('rappers').deleteOne({stageName: request.body.stageNameS})
+    .then(result => {
+        console.log('Rapper Deleted')
+        response.json('Rapper Deleted')
+    })
+    .catch(error => console.error(error))
+
+})
+
+//send json back; /api:queryParameter
+// app.get ('/api/:philoName', (req, res)=>{ //get url and query param
+//     const philoName = req.params.philoName.toLowerCase() //assign query param to variable
+//     if(philoList[philoName]){ //if List contains query param variable (philosopher last name)
+//         res.json(philoList[philoName])    //respond with corresponding object
+//     }else{ //if List contains no such name, res with unknown object.
+//         res.json(philoList['unknown'])
+//     }
+// })
+
 app.listen(PORT, ()=>{
-    console.log(`server is running on port ${PORT}`) //backticks to make ${} work
+    console.log(`server is running on port ${PORT}`) //back ticks to make ${} work
 })
